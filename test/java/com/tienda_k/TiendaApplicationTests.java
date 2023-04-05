080eb4d3c7ee85ddb34efb16d56b1f143f1552c6
zusa2001 <45644727+zusa2001@users.noreply.github.com>
2023-04-05 14:16:43 -0600

    Add files via upload

---
+++ test/java/com/tienda_k/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
